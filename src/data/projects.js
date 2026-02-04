6bd161408c8a6f4a68b9eb6a89c039fdc6a52354
Barbara Goldbeck <goldbeck.barbara@gmail.com>
2026-02-04 16:25:03 +0100

    added folders and project.js, fixed issue with aid of chatbot with npm run dev runnning error

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Mindful Moments - MERN App",
+        image: "",
+        tech: ["React", "Node", "Express", "MongoDB", "Tailwind", "DaisyUI"],
+        description: "Fullstack bootcamp cap-stone project mit CRUD, Auth und Dashboard",
+        live: "https://my-mindful-moment.netlify.app", 
+        code: ""
+    },
+    {
+        id:2, 
+        title: "Nature's Jewels - Collaborative Project",
+        image:"",
+        tech:[ "React", "Node", "Tailwind", "DaisyUI"], 
+        description: "Social App to collect and share spots in nature. CRUD functions, Login, ",
+        live: "https://natures-jewels.netlify.app",
+        code: ""
+    },
+    {
+        id:3,
+        title: "Little Mushroom Picker - Browsergame",
+        image:"",
+        tech: ["HTML", "CSS", "JavaScript"],
+        description: "Game-Loop, Collision Detection, localStorage, Animation, Score",
+        live: "https://littlemushroompicker.netlify.app",
+        code: ""
+    }
+]
